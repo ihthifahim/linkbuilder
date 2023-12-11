dd90449b29a525ee200c8d2d426cff2bfefd0295
Ihthishaam Fahim <emailme@ihthishaam.com>
2023-12-10 20:54:59 -0500

    added cors

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors');
 const dotenv = require('dotenv')
 const sequelize = require('./config/sequelize');
 
@@ -6,7 +7,7 @@ dotenv.config();
 
 const app = express();
 
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
