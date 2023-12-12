ab0354d1e2e10866f981b1e11280bc3b7f06f253
Ihthishaam Fahim <emailme@ihthishaam.com>
2023-12-12 09:37:06 -0500

    updates

---
+++ backend/db/models/Sessions.js
@@ -0,0 +1,8 @@
+const { DataTypes } = require('sequelize');
+const cuid = require('cuid');
+const sequelize = require('../../config/sequelize');
+const User = require('./user.model');
+
+const Sessions = sequelize.define('userSessions', {
+
+})
