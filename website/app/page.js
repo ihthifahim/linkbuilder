14580c1e48cbfc7c33a79aff3362b807d21e605f
Ihthishaam Fahim <emailme@ihthishaam.com>
2023-12-15 13:55:24 -0500

    added page

--- website/app/page.js
+++ website/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
       <main className="flex justify-center items-center min-h-screen bg-black">
         <div className="relative">
-            <div className="absolute blur-[40px] opacity-30 bg-purple-600 w-full p-10 mt-10 "></div>
+            <div className="absolute blur-[40px] opacity-30 bg-purple-600 w-full p-10 mt-10"></div>
             <h1 className="font-bold text-8xl bg-gradient-to-r from-20% bg-clip-text text-transparent from-purple-500 to-purple-200 py-5">Chewingg...</h1>
         </div>
       </main>
