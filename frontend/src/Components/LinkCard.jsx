1e3843042553b0381a8932b2656c1d103f9a5843
Ihthishaam Fahim <emailme@ihthishaam.com>
2023-12-17 19:02:13 -0500

    url country traffic not recording

--- frontend/src/Components/LinkCard.jsx
+++ frontend/src/Components/LinkCard.jsx
@@ -73,7 +73,7 @@ export default function LinkCard({link}){
 
     return(
         <>
-            <Link to={`/manage/links/${link.link_key}`}>
+            {/*<Link to={`/manage/links/${link.link_key}`}>*/}
             <div className="bg-white shadow-sm rounded-lg py-5 px-5 flex items-center hover:shadow-lg mb-5">
                 <div className="">
                     <img alt="profile Image" src={link.page_favicon} className="w-16 rounded-full" />
@@ -81,7 +81,11 @@ export default function LinkCard({link}){
                 <div className="w-full ml-5">
                     <h2 className="font-bold text-lg text-blue-600" ref={linkRef}>
                         gum.lk/{link.link_key}
-                        <button onClick={handleCopyToClipboard} className=" ml-3 group rounded-full bg-gray-100 p-1.5 transition-all duration-75 hover:scale-105 hover:bg-blue-100 active:scale-95"><span className="sr-only"></span><svg fill="none" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="14" height="14" className="text-gray-700 transition-all group-hover:text-blue-800"><path d="M8 17.929H6c-1.105 0-2-.912-2-2.036V5.036C4 3.91 4.895 3 6 3h8c1.105 0 2 .911 2 2.036v1.866m-6 .17h8c1.105 0 2 .91 2 2.035v10.857C20 21.09 19.105 22 18 22h-8c-1.105 0-2-.911-2-2.036V9.107c0-1.124.895-2.036 2-2.036z"></path></svg></button>
+                        <button onClick={handleCopyToClipboard} className=" ml-3 group rounded-full bg-gray-100 p-1.5 transition-all duration-75 hover:scale-105 hover:bg-blue-100 active:scale-95"><span className="sr-only"></span><svg fill="none" shapeRendering="geometricPrecision"
+                                                                                                                                                                                                                                          stroke="currentColor"
+                                                                                                                                                                                                                                          strokeLinecap="round"
+                                                                                                                                                                                                                                          strokeLinejoin="round"
+                                                                                                                                                                                                                                          strokeWidth="1.5" viewBox="0 0 24 24" width="14" height="14" className="text-gray-700 transition-all group-hover:text-blue-800"><path d="M8 17.929H6c-1.105 0-2-.912-2-2.036V5.036C4 3.91 4.895 3 6 3h8c1.105 0 2 .911 2 2.036v1.866m-6 .17h8c1.105 0 2 .91 2 2.035v10.857C20 21.09 19.105 22 18 22h-8c-1.105 0-2-.911-2-2.036V9.107c0-1.124.895-2.036 2-2.036z"></path></svg></button>
                     </h2>
                     <a href={link.destinationURL} className="text-xs text-gray-600 hover:text-black hover:underline">{truncatedURL}</a>
                     <p className="text-xs text-gray-400 mt-1">Created {timeElapsed}</p>
@@ -92,7 +96,7 @@ export default function LinkCard({link}){
                     </div>
                 </div>
             </div>
-            </Link>
+
 
             {isCopied && <LinkCopied />}
 
