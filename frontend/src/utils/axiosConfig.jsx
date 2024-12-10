af311b7cbdf084eb5ae6f75af5851e7510b58f76
Ihthishaam Fahim <emailme@ihthishaam.com>
2024-12-09 19:51:55 -0500

    token error

--- frontend/src/utils/axiosConfig.jsx
+++ frontend/src/utils/axiosConfig.jsx
@@ -11,5 +11,13 @@ const axiosInstance = axios.create({
     }
 })
 
+axiosInstance.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.Authorization = `${token}`;
+    }
+    return config;
+});
+
 
 export default axiosInstance;
